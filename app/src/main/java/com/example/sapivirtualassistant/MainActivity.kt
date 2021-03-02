fe1f32e5e2d1be88c5bf9c0059a1bc0dc58d1394
FranciskaNemeth <franciska99n@gmail.com>
2021-03-02 11:18:13 +0200

    deleted redundant code

--- app/src/main/java/com/example/sapivirtualassistant/MainActivity.kt
+++ app/src/main/java/com/example/sapivirtualassistant/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        setContentView(R.layout.feedback_screen)
+        setContentView(R.layout.activity_main)
     }
 }
