8ec36b124a0a739a19cbee886b735ccac112873d
FranciskaNemeth <franciska99n@gmail.com>
2021-03-24 20:49:49 +0200

    added birthday picker to profile

--- app/src/main/java/com/example/sapivirtualassistant/ProfileFragment.kt
+++ app/src/main/java/com/example/sapivirtualassistant/ProfileFragment.kt
@@ -1,12 +1,22 @@
 package com.example.sapivirtualassistant
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ProfileFragment : Fragment() {
+
+class ProfileFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+    lateinit var datePickerButton : ImageView
+    lateinit var datePickerText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,6 +26,41 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_screen, container, false)
+        val view = inflater.inflate(R.layout.profile_screen, container, false)
+
+        datePickerButton = view.findViewById(R.id.imageViewCalendar)
+        datePickerText = view.findViewById(R.id.editTextBirthDate)
+
+        datePickerButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        datePickerText.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        return view
+    }
+
+    fun showDatePickerDialog() {
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            this,
+            Calendar.getInstance().get(Calendar.YEAR),
+            Calendar.getInstance().get(Calendar.MONTH),
+            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        /*val date = "$year.$month.$dayOfMonth"
+        datePickerText.setText(date)*/
+
+        val sdf = SimpleDateFormat("yyyy.MMMM.dd")
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        val dateString: String = sdf.format(calendar.time)
+        datePickerText.setText(dateString)
     }
 }
