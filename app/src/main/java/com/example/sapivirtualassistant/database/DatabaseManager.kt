a8af53c0a1ff498bc04f80afa372fad2d6cdbe99
FranciskaNemeth <franciska99n@gmail.com>
2021-04-13 22:15:02 +0300

    added help screen implementation

--- app/src/main/java/com/example/sapivirtualassistant/database/DatabaseManager.kt
+++ app/src/main/java/com/example/sapivirtualassistant/database/DatabaseManager.kt
@@ -1,13 +1,17 @@
 package com.example.sapivirtualassistant.database
 
 import android.util.Log
+import com.example.sapivirtualassistant.interfaces.GetHelpModelInterface
 import com.example.sapivirtualassistant.interfaces.GetUserInterface
+import com.example.sapivirtualassistant.model.HelpModel
 import com.example.sapivirtualassistant.model.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 object DatabaseManager {
     lateinit var user : User
+    lateinit var helpModel: HelpModel
+    lateinit var helpList : MutableList<HelpModel>
 
     fun getUserData(email : String, getUserInterface : GetUserInterface? = null) {
         Firebase.firestore.collection("users").document(email)
@@ -44,5 +48,26 @@ object DatabaseManager {
                     classGroup = map["classGroup"] as String?,
                     teacherTimeTable = map["teacherTimeTable"] as String?
         )
+    }
+
+    fun getHelpData(getHelpModelInterface: GetHelpModelInterface? = null) {
+        Firebase.firestore.collection("help")
+            .get()
+            .addOnSuccessListener { document ->
+                helpList = ArrayList()
+                for (doc in document.documents) {
+                    helpModel = mapToHelpModel(doc.data!!)
+                    helpList.add(helpModel)
+                }
+
+                getHelpModelInterface?.getHelpModel(helpList)
+            }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "Error getting documents: ", exception)
+            }
+    }
+
+    private fun mapToHelpModel(map : MutableMap<String, Any>) : HelpModel {
+        return HelpModel(question = map["q"].toString(), answer = map["a"].toString(), type = map["type"].toString())
     }
 }
